de8a85ebdca06c8bd0645cd4fa441066253b14b3
Federico Jure <fjure@atixlabs.com>
2022-06-24 15:18:20 -0300

    se llama a la api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Buscador from './componentes/Buscador';
 import Capitulo from './componentes/Capitulo'
 
-
+//92749
 function App() {
 
-  const [cap, setCap] = useState(null)
+  
+  const apikey = 'api_key=133f4d8b4fed128b27fa0bb407956c56'
+  const base = 'https://api.themoviedb.org/3'
+  const fetchApi = async () => {
+    const response = await fetch(`${base}/tv/92749?${apikey}&language=es-ES`)
+    console.log(response.statusText)
+  }
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
 
   const searchEpisode = (number) => {
     console.log(number)
